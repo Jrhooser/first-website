a3a0367ebffe4ae23b94c7a5683539d3494b0bc9
Jacob Hooser <jacob.hooser@yahoo.com>
2022-02-18 15:05:18 -0600

    added list and mario button

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "I have no idea what to put on this website";
 document.querySelector("body").appendChild(h2);
